b2374c478788b932744d0b13585ffae95c1c6f7d
Your Name <you@example.com>
2023-05-15 15:56:11 +0800

    py37

---
+++ run.sh
@@ -0,0 +1,27 @@
+python -m torch.distributed.launch --nproc_per_node=8 --master_port=20002 \
+    fastchat/train/train.py \
+    --model_name_or_path llama-7b-hf/  \
+    --data_path playground/data/dummy.json \
+    --bf16 False \
+    --bf16_full_eval False \
+    --fp16 True \
+    --fp16_full_eval False \
+    --fp16_opt_level O2 \
+    --output_dir ./output \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 16 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 1200 \
+    --save_total_limit 10 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --deepspeed ./scripts/deepspeed.config \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --lazy_preprocess True
